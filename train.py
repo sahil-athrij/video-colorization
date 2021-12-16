173769645e942d8213052eb5dbdc3b2f82e76f75
Sahil athrij <sahilathrij@gmail.com>
2021-12-16 23:54:15 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 from os import listdir
 
+import numpy as np
 import torch
 import torch.optim as optim
 from cv2 import cv2
 from torch import nn
+import cupy
 
 from torch.utils.data import DataLoader, Dataset
 from model import load_model, preprocess_img, preprocess_img_ab
@@ -21,21 +23,22 @@ class MyDataset(Dataset):
         y = []
 
         for i in range(len(listdir(root))):
-            image = cv2.imread(f"{root}/{i}.png")
+            image = cupy.asarray(cv2.imread(f"{root}/image-{i}.png"))
             original, image_processed = preprocess_img(image)
 
             x.append(image_processed)
-            y.append(preprocess_img_ab(image)[1])
+            y.append(torch.swapaxes(preprocess_img_ab(image)[1],1,3))
 
         self.X = x
         self.Y = y
 
     def __len__(self):
+
         return len(self.X)
 
     def __getitem__(self, idx):
         im = self.X[idx]
-        label = torch.cat((self.Y[idx - 1 if idx != 0 else 0], self.Y[idx]), dim=1)[0]
+        label = self.Y[idx]
         im_prev = im if idx == 0 else self.X[idx - 1]
 
         if self.transformX is not None:
@@ -48,7 +51,7 @@ class MyDataset(Dataset):
 
 
 def train(pix, generator, discriminator, batch_size=1):
-    train_loader = torch.utils.data.DataLoader(MyDataset(root="input/x"), batch_size=batch_size, shuffle=True,
+    train_loader = torch.utils.data.DataLoader(MyDataset(root="../output/8/x"), batch_size=batch_size, shuffle=True,
                                                num_workers=0, drop_last=True)
 
     pix.cuda()
@@ -62,7 +65,6 @@ def train(pix, generator, discriminator, batch_size=1):
     discriminator_loss = nn.MSELoss()
 
     TRUE = torch.zeros([1, 1, 256, 256], dtype=torch.float).cuda()
-    FALSE = torch.ones([1, 1, 256, 256], dtype=torch.float).cuda()
 
     device = torch.device('cuda:0')
 
@@ -71,6 +73,7 @@ def train(pix, generator, discriminator, batch_size=1):
         for images, label in train_loader:
             img = images[0].to(device)[0]
             prev = images[1].to(device)[0]
+            label = label[0].to(device)
 
             generator_optimiser.zero_grad()
             discriminator_optimiser.zero_grad()
@@ -78,24 +81,28 @@ def train(pix, generator, discriminator, batch_size=1):
             pix1 = pix(prev)
             pix2 = pix(img)
 
-            output = generator(pix1, pix2)
+            output = generator(pix1, pix2).detach()
+            print(output.shape)
+
+            output_fake = discriminator(output)
+
+            FALSE = torch.ones([1, 1, 256, 256], dtype=torch.float).cuda()
+            loss_gen = generator_loss(output- pix1, label-label_1) + discriminator_loss(output_fake, FALSE)
+            loss_gen.backward()
+            generator_optimiser.step()
+
+            FALSE = torch.ones([1, 1, 256, 256], dtype=torch.float).cuda()
             output_fake = discriminator(output)
             output_real = discriminator(label)
 
-            loss_gen = generator_loss(output, label)
+            # real_x - real_x_1 ~= fake_x - fake_x_1
             loss_des = discriminator_loss(output_fake, FALSE) + discriminator_loss(output_real, TRUE)
-
-            loss_gen += (1 - loss_des)
-
-            loss_gen.backward(retain_graph=True)
-            loss_des.backward(retain_graph=True)
-
-            generator_optimiser.step()
+            loss_des.backward()
             discriminator_optimiser.step()
 
             total_loss += loss_gen
 
-        print(f"Epoch {epoch} Loss {total_loss/len(train_loader)}")
+        print(f"Epoch {epoch} Loss {total_loss / len(train_loader)}")
 
 
 if __name__ == "__main__":
