ed728a2b980c90c2c2f85a026670e5689d316d06
Rohit <45072928+rohittp0@users.noreply.github.com>
2021-12-16 15:21:40 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,18 +1,20 @@
 from os import listdir
 
 import torch
+import torch.optim as optim
 from cv2 import cv2
+from torch import nn
 
 from torch.utils.data import DataLoader, Dataset
-from model import load_model, preprocess_img, preprocess_img_ab, postprocess_tens
+from model import load_model, preprocess_img, preprocess_img_ab
 from network import Model
 
 
 class MyDataset(Dataset):
 
-    def __init__(self, root, transformX=None, transformY=None):
-        self.transformX = transformX
-        self.transformY = transformY
+    def __init__(self, root, transform_x=None, transform_y=None):
+        self.transformX = transform_x
+        self.transformY = transform_y
 
         x = []
         y = []
@@ -45,17 +47,25 @@ class MyDataset(Dataset):
 
 
 def train(pix, generator, batch_size=8):
-    train_loader = torch.utils.data.DataLoader(MyDataset(root="input/sunith"), batch_size=batch_size, shuffle=True,
-                                              num_workers=2, drop_last=True)
+    train_loader = torch.utils.data.DataLoader(MyDataset(root="input/x"), batch_size=batch_size, shuffle=True,
+                                               num_workers=2, drop_last=True)
+
+    generator_optimiser = optim.SGD(generator.parameters(), lr=0.03)
+    generator_loss = nn.MSELoss()
 
     for epoch in range(5):
         for images, label in train_loader:
+            generator_optimiser.zero_grad()
+
             pix1 = pix(images[0])
             pix2 = pix(images[1])
 
             output = generator(pix1, pix2)
 
-            print(output.shape)
+            loss = generator_loss(output, label)
+
+            loss.backward()
+            generator_optimiser.step()
 
 
 if __name__ == "__main__":
